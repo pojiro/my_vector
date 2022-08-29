5508fec527fdebdaaa3da2bbd83ffc88a1cbd1e8
pojiro <pojiro.jp@gmail.com>
2022-08-30 08:28:13 +0900

    new 式

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include "my_vector.hpp"
 
 int main(int argc, char *argv[]) {
-  void *ptr = ::operator new(sizeof(my_vector<int>));
-  auto v_ptr = new (ptr) my_vector<int>{};
+  auto v_ptr = new my_vector<int>{};
 
   assert(v_ptr->size() == 0);
   return 0;
