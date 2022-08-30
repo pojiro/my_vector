3390aa026e6b4082c3c76fefc927fef63d214198
pojiro <pojiro.jp@gmail.com>
2022-08-30 09:10:47 +0900

    add size_

--- my_vector.hpp
+++ my_vector.hpp
@@ -6,5 +6,8 @@ template <typename T> class my_vector {
 public:
   my_vector() { std::cout << "constructed \n"s; };
 
-  std::size_t size() { return 0; };
+  std::size_t size() { return size_; };
+
+private:
+  std::size_t size_;
 };
