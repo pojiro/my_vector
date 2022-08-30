f9e93f79ad28345d935b06aa8f86ac3df7d99134
pojiro <pojiro.jp@gmail.com>
2022-08-30 10:11:57 +0900

    add type aliases

--- my_vector.hpp
+++ my_vector.hpp
@@ -3,24 +3,30 @@
 using namespace std::literals;
 
 template <typename T, typename Allocator = std::allocator<T>> class my_vector {
-  using traits = std::allocator_traits<Allocator>;
-
 public:
-  my_vector(std::size_t size = 0, Allocator allocator = Allocator())
+  using value_type = T;
+  using pointer = T *;
+  using reference = T &;
+  using allocator_type = Allocator;
+  using size_type = std::size_t;
+
+  my_vector(size_type size = 0, allocator_type allocator = allocator_type())
       : allocator_(allocator), size_(size) {
     pointer_ = traits::allocate(allocator_, size_);
   }
 
-  std::size_t size() { return size_; }
+  size_type size() { return size_; }
 
-  T &front() { return *pointer_; }
+  reference front() { return *pointer_; }
 
-  T &operator[](std::size_t index) { return *(pointer_ + index); }
+  reference operator[](size_type index) { return *(pointer_ + index); }
 
   ~my_vector() { traits::deallocate(allocator_, pointer_, size_); }
 
 private:
-  T *pointer_;
-  Allocator allocator_;
-  std::size_t size_;
+  using traits = std::allocator_traits<allocator_type>;
+
+  pointer pointer_;
+  allocator_type allocator_;
+  size_type size_;
 };
