7c59e4948ea5d1119e6d8cb6b36968ed1df8acbb
pojiro <pojiro.jp@gmail.com>
2022-08-30 09:54:15 +0900

    remove unnecessary semicolon

--- my_vector.hpp
+++ my_vector.hpp
@@ -11,11 +11,11 @@ public:
   my_vector(std::size_t size = 0, Allocator allocator = Allocator())
       : allocator_(allocator), size_(size) {
     pointer_ = traits::allocate(allocator_, size_);
-  };
+  }
 
-  std::size_t size() { return size_; };
+  std::size_t size() { return size_; }
 
-  ~my_vector() { traits::deallocate(allocator_, pointer_, size_); };
+  ~my_vector() { traits::deallocate(allocator_, pointer_, size_); }
 
 private:
   T *pointer_;
